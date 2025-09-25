92a0c970badb46ba8077a14ac59eca382ee351fb
SantosNovoa <novoasj8729@student.laccd.edu>
2025-09-24 20:49:44 -0400

    Initial commit of squibislands lorekeeper setup

--- resources/views/user/_profile_content.blade.php
+++ resources/views/user/_profile_content.blade.php
@@ -28,19 +28,11 @@
 
         <!-- User Information -->
         <div class="row no-gutters">
-<<<<<<< HEAD
-            <div class="row no-gutters col-sm-5">
-                <div class="col-lg-3 col-md-3 col-4">
-                    <h5>Alias</h5>
-                </div>
-                <div class="col-lg-9 col-md-9 col-8">
-=======
             <div class="row col-sm-6">
                 <div class="col-md-3 col-4">
                     <h5>Alias</h5>
                 </div>
                 <div class="col-md-9 col-8">
->>>>>>> preimpression/v3/world-expansion
                     {!! $user->displayAlias !!}
                     @if (count($aliases) > 1 && config('lorekeeper.extensions.aliases_on_userpage'))
                         <a class="small collapse-toggle collapsed" href="#otherUserAliases" data-toggle="collapse">&nbsp;</a>
@@ -54,28 +46,13 @@
                     @endif
                 </div>
             </div>
-<<<<<<< HEAD
-            <div class="row no-gutters col-sm-7">
-                <div class="col-md-4 col-4">
-=======
             <div class="row col-sm-6">
                 <div class="col-md-3 col-4">
->>>>>>> preimpression/v3/world-expansion
                     <h5>Joined</h5>
                 </div>
                 <div class="col-md-8 col-8">{!! format_date($user->created_at, false) !!} ({{ $user->created_at->diffForHumans() }})</div>
             </div>
-<<<<<<< HEAD
-            <div class="row no-gutters col-sm-5">
-                <div class="col-lg-3 col-md-3 col-4">
-                    <h5>Rank</h5>
-                </div>
-                <div class="col-lg-9 col-md-9 col-8">{!! $user->rank->displayName !!} {!! $user->rank->parsed_description ? add_help($user->rank->parsed_description) : '' !!}</div>
-            </div>
-            @if ($user->birthdayDisplay && isset($user->birthday))
-                <div class="row no-gutters col-sm-7">
-                    <div class="col-md-4 col-4">
-=======
+
             <div class="row col-sm-6">
                 <div class="col-md-3 col-4">
                     <h5>Rank</h5>
@@ -85,7 +62,6 @@
             @if ($user->birthdayDisplay && isset($user->birthday))
                 <div class="row col-sm-6">
                     <div class="col-md-3 col-4">
->>>>>>> preimpression/v3/world-expansion
                         <h5>Birthday</h5>
                     </div>
                     <div class="col-md-8 col-8">{!! $user->birthdayDisplay !!}</div>
